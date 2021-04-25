375a9687f29adcbde2e096f876c26c3b2aab3e73
Abu Kuhafah s3839222 <s3839222@student.rmit.edu.au>
2021-04-25 11:42:38 +1000

    some implementation of linkedlist

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,5 +1,6 @@
 
 #include "LinkedList.h"
+#include <stdexcept>
 
 LinkedList::LinkedList() {
    head = nullptr;
@@ -8,4 +9,79 @@ LinkedList::LinkedList() {
 }
 
 LinkedList::~LinkedList() {
+   clear();
+}
+
+int LinkedList::size(){
+    
+    int length = 0;
+
+    Node* current = head;
+    while(current != nullptr){
+        ++length;
+        current = current->next;
+    }
+    return length;
+}
+
+// void LinkedList::add_back(Tile* data){
+//     Node* node = new Node();
+//     node->tile = data;
+//     node->next = nullptr;
+
+//     if(head == nullptr){
+//         head = node;
+//     }else{
+//         Node* current = head;
+//         while(current->next != nullptr){
+//             current = current->next;
+//         }
+//         current->next = node;
+//     }
+
+// }
+
+void LinkedList::remove(int index){
+    if(index >= 0 && index < size()){
+        if(head != nullptr){
+            int counter = 0;
+            Node* current = head;
+            
+            Node* prev = nullptr;
+
+            while(counter != index){
+                ++counter;
+                prev = current;
+                current = current->next;
+            }
+
+            if(prev == nullptr){
+                head = current->next;
+            }else{
+                prev->next = current->next;
+            }
+
+            delete current->tile;
+            delete current;
+        }
+    }
+}
+
+void LinkedList::remove_front(){
+    if(head != nullptr){
+        Node* toDelete = head;
+        head = head->next;
+
+        delete toDelete->tile;
+        delete toDelete;
+    }else{
+        throw std::runtime_error("Nothing to remove");
+    }
+
+}
+
+void LinkedList::clear(){
+    while(head != nullptr){
+        remove_front();
+    }
 }
--- LinkedList.h
+++ LinkedList.h
@@ -9,6 +9,11 @@ public:
 
    LinkedList();
    ~LinkedList();
+   int size();
+   void clear();
+   void add_back(Tile* data);
+   void remove(int index);
+   void remove_front();
 
 private:
    Node* head;
