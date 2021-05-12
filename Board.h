531ea86adc0d54d7cf44312a92ac8fd4e56c6b35
Alasdair Moloney <s3785526@student.rmit.edu.au>
2021-05-12 23:11:44 +1000

    Merged board implementation and master

--- Board.h
+++ Board.h
@@ -4,17 +4,28 @@
 
 #include <vector>
 #include <string>
+#include <cmath>
+
 #include "Tile.h"
+
+#define COLUMN_SIZE     26
+#define ROW_SIZE        26
+#define BOARD_SIZE      26
 
 class Board {
 public:
 
-   void addToBoard(Tile* t);
-   int calcScoreFrom(std::string pos);
+   void addToBoard(Tile* t, std::string pos);
+   int calcScoreFrom(std::string pos, Tile* g);
    bool legalPlacementAt(std::string pos, Tile* t);
-   std::vector<std::vector<Tile*>> getBoard();
+   bool islegalVerticalCheck(int x, int y, Tile* t);
+   bool islegalHorizontalCheck(int x, int y, Tile* t);
    int getWidth();
-   char getHeight();
+   int getHeight();
+   int getMaxRow();
+   int getMaxCol();
+   void displayBoard();
+   std::vector<std::vector<Tile*>> getBoard();
 
 private:
    std::vector<std::vector<Tile*>> board;
