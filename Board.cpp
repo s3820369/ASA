70080c5b1d9f8d89820c9077523ed3f80dc441f9
s3847243 <s3847243@student.rmit.edu.au>
2021-05-16 01:57:37 +1000

    points method updated

--- Board.cpp
+++ Board.cpp
@@ -19,16 +19,15 @@ void Board::addToBoard(Tile* t, int x, int y) {
 }
 
 
-int Board::calcScoreFrom(int x, int y, Tile* g) {
+int Board::calcScoreFrom(int col, int row, Tile* g) {
     // adding the points from the row
     // adding the points for columns
     //checking if sthers a qwirkle
     int i;
-    char row = y;
-    int rowDown = 'Z' - row;
+    //char row = y;
+    //int rowDown = row - 'A';
    // int rowRight = abs(row - 'A');
-    char col = x;
-    int ColNum = col - '0';
+    //char col = x;
     int scoreRowUp = 0;
     int scoreRowDown= 0;
     int scoreColRight = 0;
@@ -36,10 +35,9 @@ int Board::calcScoreFrom(int x, int y, Tile* g) {
     bool stop = false;
     
     while(!stop){
-        for(i = row;i<='Z';i++){        //rowDown
+        for(i = row;i<=BOARD_SIZE;i++){        
             
-            int h = 'Z' - i;
-            if(board[h][ColNum] != nullptr){
+            if(board[i][col] != nullptr){
                 scoreRowDown++;
             }else{
                 stop = true;
@@ -48,9 +46,9 @@ int Board::calcScoreFrom(int x, int y, Tile* g) {
     }
     stop = false;
     while(!stop){
-        for(i = row - 1;i<='A';i--){      //rowUp
-            int f = i - 'A';
-                if(board[f][ColNum] != nullptr){
+        for(i = row;i<=0;i--){      //rowUp
+            
+                if(board[i][col] != nullptr){
                     scoreRowUp++;
                 }else{
                     stop = true;
@@ -58,11 +56,11 @@ int Board::calcScoreFrom(int x, int y, Tile* g) {
             
         }
     }
-    int totalRowScore = scoreRowDown + scoreRowUp;
+    int totalRowScore = scoreRowDown + scoreRowUp - 1;   //change 1 with a define statement called duplicate
     stop = false;
     while(!stop){
-        for(int g = ColNum;g< BOARD_SIZE;g++){
-            if(board[rowDown][ColNum] != nullptr){
+        for(int g = col;g< BOARD_SIZE;g++){
+            if(board[row][g] != nullptr){
                 scoreColRight++;
             }else{
                 stop = true;
@@ -72,25 +70,34 @@ int Board::calcScoreFrom(int x, int y, Tile* g) {
     stop = false;
     while (!stop)
     {
-        for(int a = ColNum - 1;a<=0;a--){
-            if(board[rowDown][ColNum] != nullptr){
+        for(int a = col;a<=0;a--){
+            if(board[row][a] != nullptr){
                 scoreColLeft++;
             }else{
                 stop = true;
             }
         }
     }
-    int tot = totalRowScore + scoreColLeft + scoreColRight;
+    int totalColumnScore = scoreColLeft + scoreColRight - 1 ;   //change 1 with a define statement called duplicate
+    int tot = totalRowScore + totalColumnScore;
+    /**
+     * if(totalRowScore || totalColumnScore){
+     * std::cout<<"Qwirkle bois" << std::endl;
+     *  }
+     * 
+    */
+
     return tot;
 
 
 }
 
 
+
 bool Board::legalPlacementAt(int x, int y, Tile* tile) {
     return isLegalVerticalCheck(tile, x, y, 0, ROWS)      &&
            isLegalHorizontalCheck(tile, x, y, 0, COLUMNS) &&
-           nullptr == board[row][col];
+           nullptr == board[y][x];
 }
 
 bool Board::isLegalVerticalCheck(Tile* placing, int x, int y, int lower, int upper) {
